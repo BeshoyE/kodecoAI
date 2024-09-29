674840c1d8fe99c35d291d9051ed70c7dc43160a
Beshoy Eskarous <beshoyesk@gmail.com>
2024-09-28 17:59:32 -0700

    initial commit on week1, empty file

---
+++ week1HW/week1HW/week1HW/week1HWApp.swift
@@ -0,0 +1,17 @@
+//
+//  week1HWApp.swift
+//  week1HW
+//
+//  Created by Beshoy Eskarous on 9/28/24.
+//
+
+import SwiftUI
+
+@main
+struct week1HWApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ week1HW/week1HW/week1HWTests/week1HWTests.swift
@@ -0,0 +1,17 @@
+//
+//  week1HWTests.swift
+//  week1HWTests
+//
+//  Created by Beshoy Eskarous on 9/28/24.
+//
+
+import Testing
+@testable import week1HW
+
+struct week1HWTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
